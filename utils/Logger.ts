a9e6378ad13bfd050af776b4975fadfa657a8611
Dharmendra Soni <dharmendrashaheagle@gmail.com>
2022-08-11 14:06:06 +0530

    1 introduce file logger (#4)
    * feat(1-introduce-file-logger): enabled file logging
    
    * feat(1-introduce-file-logger): ignored *.log files
    
    * feat(1-introduce-file-logger): updated autometic creation of folders and files

--- utils/Logger.ts
+++ utils/Logger.ts
@@ -5,12 +5,31 @@ import { Logger } from '../node_modules/@adonisjs/logger/build/src/Logger'
 
 export const logFilePath = () => {
   const date = new Date()
-  const filename = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}.log`
-  return path.resolve(__dirname, '..', 'logs', filename)
+  const addNewFolderNames = `${date.getFullYear()}/${date.getMonth() + 1}`
+  const abssolutePathFolder = path.resolve(__dirname, '..', 'logs', addNewFolderNames)
+
+  const fileName = `${date.getDate()}.log`
+
+  fs.mkdirSync(abssolutePathFolder, { recursive: true })
+
+  const absolutePath = path.resolve(abssolutePathFolder, fileName)
+
+  fs.appendFile(absolutePath, '', (err) => {
+    if (err) {
+      console.error(err.message, err)
+      process.exit(1)
+    }
+  })
+
+  return absolutePath
 }
 
-export const writeFile = () => fs.createWriteStream(logFilePath())
-
+export const writeFile = () =>
+  fs.createWriteStream(logFilePath(), {
+    flags: 'a+',
+    encoding: undefined,
+    mode: 0o777,
+  })
 const PinoLogger = Pino(
   {
     prettyPrint: {
@@ -19,7 +38,7 @@ const PinoLogger = Pino(
       translateTime: 'yyyy-dd-mm, h:MM:ss TT',
     },
   },
-  Pino.destination(logFilePath())
+  Pino.destination(writeFile())
 )
 
 const logger = new Logger(
