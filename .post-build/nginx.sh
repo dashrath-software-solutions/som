e9e96b76bab975541290a821f2f9634b854ffe2d
dharmendrasha <dharmendrashaheagle@gmail.com>
2022-08-27 08:44:41 +0000

    feat(14-introduce-mono-repo): added monorepo

--- .post-build/nginx.sh
+++ .post-build/nginx.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd ..
-sudo cp .nginx/site /etc/nginx/sites-available/default
-sudo cp .nginx/proxy_params /etc/nginx/proxy_params
-sudo /etc/init.d/nginx restart
+cd .nginx &&
+    pwd &&
+    ls -al &&
+    sudo cp nginx.conf /etc/nginx/sites-available/default &&
+    sudo cp proxy_params /etc/nginx/proxy_params &&
+    sudo nginx -t &&
+    sudo /etc/init.d/nginx restart &&
+    sudo /etc/init.d/nginx status &&
+    sudo corepack enable &&
+    pnpm install
